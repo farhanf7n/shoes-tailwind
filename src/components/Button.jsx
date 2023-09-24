5906f2cf18162682362f06a5113e57aa1b5f62a9
Farhan Ahmed <farhanf7n@gmail.com>
2023-09-24 23:27:12 +0400

    Made Shoe Card

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,31 @@
-const Button = () => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
 
-export default Button
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
